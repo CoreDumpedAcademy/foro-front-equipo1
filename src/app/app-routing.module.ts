3e814fea682b508821ae5dcdf7c5cfa35cdc7d70
lucas-santodomingo <l.santodomingo@alumnos.upm.es>
2019-12-09 10:11:33 +0100

    register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent} from './register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+    
+  }
+  
 
 
-const routes: Routes = [];
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
